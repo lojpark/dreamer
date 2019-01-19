adc71a9778804fe974502b30c33b4656e1b522d5
lojpark <lojpark@gmail.com>
2019-01-19 15:59:15 +0900

    Apply album layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Album from './Album';
 
 class App extends Component {
   render() {
 		return (
 			<div className="App">
-      
+        <Album></Album>
       </div>
     );
   }

d7ac61ad0458cf8ea351cf8063d33426268f87f8
TanDung2512 <36843017+TanDung2512@users.noreply.github.com>
2019-01-28 12:03:01 +0700

    Add files via upload
    features : login and authentication .
                     if fail , ... a pop-up alert message to announce fail and turn back to sign in component

--- src/components/signin/BasicSignInForm.js
+++ src/components/signin/BasicSignInForm.js
@@ -1,23 +1,66 @@
 import React,{Component} from 'react';
 import './BasicSignInForm.css';
+import { connect } from 'react-redux';
+import { signin } from "../../store/actions/authSignInActions";
+
+//html message fail login
+
+
 class BasicSignInForm extends Component {
+    constructor(props){
+        super(props);
+        this.handleChange=this.handleChange.bind(this);
+        this.login = this.login.bind(this);
+        this.state = {
+            email:'',
+            password: ''
+        }
+    }
+
+    login(e){
+        e.preventDefault();
+        this.props.signin(this.state);
+
+    }
+    handleChange(event){
+        console.log(event);
+        this.setState({[event.target.name]: event.target.value});
+
+    }
+
+
     render(){
+            const { authSignInError } = this.props;
+            const PopUpMessageFailLogin =
+            (<div className="alert alert-info" role="alert" styles = "top:46px">
+                The username and password you entered did not match our records. Please double-check and try again.
+            </div>);
         return(
+
             <div>
+
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-                <style type="text/css">
-                </style>
+                <style type="text/css"></style>
+
+
+                {/*check whether sign in is success or not .. and then pop up message if it fail*/}
+                {authSignInError ?  <div className="alert alert-info" role="alert" styles = "top:46px"  >
+                    <div className="text-center">
+                    The username and password you entered did not match our records. Please double-check and try again.
+                    </div>
+                </div> : <div></div>}
+
                 <div className="login-form">
-                    <form action="/examples/actions/confirmation.php" method="post">
+                    <form onSubmit={this.login}>
                         <h2 className="text-center">Log in</h2>
                         <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Username" required="required" />
+                            <input type="text" className="form-control" placeholder="Username" name="email" required="required" value={this.state.email} onChange={this.handleChange}/>
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Password" required="required" />
+                            <input type="password" className="form-control" placeholder="Password" name="password" required="required" value={this.state.password} onChange={this.handleChange}/>
                         </div>
                         <div className="form-group">
-                            <button type="submit" className="btn btn-primary btn-block">Log in</button>
+                            <button type="submit" className="btn btn-primary btn-block" >Log in</button>
                         </div>
                         <div className="clearfix">
                             <label className="pull-left checkbox-inline"><input type="checkbox"/> Remember me</label>
@@ -31,4 +74,11 @@ class BasicSignInForm extends Component {
         );
     }
 }
-export default BasicSignInForm;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signin: (User) => dispatch(signin(User))
+    }
+}
+
+export default connect(null,mapDispatchToProps)(BasicSignInForm);
--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,16 +1,29 @@
 import React,{Component} from 'react';
 import BasicSignInForm from './BasicSignInForm';
+import { connect } from 'react-redux';
+import withStyles from "@material-ui/core/styles/withStyles";
 
 class SignIn extends Component {
     render(){
+        const { authSignInSuccess,authSignInError } = this.props;
+
+
         return(
             <div>
-                <BasicSignInForm/>
+                {authSignInSuccess ?  <p>Success</p>  :  <BasicSignInForm authSignInError={authSignInError}/> }
             </div>
-
         );
+    };
 
 
+}
+
+const mapStateToProps= (state) =>{
+    return {
+        authSignInSuccess : state.auth.authSignInSuccess,
+        authSignInError : state.auth.authSignInError
     }
 }
-export default SignIn;
+
+
+export default (connect(mapStateToProps,null)(SignIn));

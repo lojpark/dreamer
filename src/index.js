eee433775b718c893e3c9df2ca3f7cf80cc2e15a
Joker195 <1552159@hcmut.edu.vn>
2019-01-29 14:24:54 +0700

    Print Done

--- src/index.js
+++ src/index.js
@@ -3,37 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './store/reducers/rootReducer';
-import thunk from 'redux-thunk'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
-import { reduxFirestore, getFirestore } from 'redux-firestore'
-import firebaseConfig from './config/firebaseConfig'
 
-const theme = createMuiTheme({
-    //override the theme here
-    typography: {
-        useNextVariants: true,
-    }
-});
-
-const store = createStore(rootReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-        reduxFirestore(firebaseConfig),
-        reactReduxFirebase(firebaseConfig)
-    )
-);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <MuiThemeProvider theme={theme} >
-            <App />
-        </MuiThemeProvider>
-    </Provider>
-    , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

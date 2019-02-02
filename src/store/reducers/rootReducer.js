5af5b11f13d0bf9b770cf01b612b8d4589fb98f5
TanDung2512 <36843017+TanDung2512@users.noreply.github.com>
2019-02-02 13:35:31 +0700

    Add files via upload

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'
 import albumReducer from './albumReducer'
 import authReducer from './authReducer'
+import profileReducer from './authReducer'
 
 const rootReducer = combineReducers({
     album: albumReducer,
-    auth: authReducer
+    auth: authReducer,
 })
 
 export default rootReducer;

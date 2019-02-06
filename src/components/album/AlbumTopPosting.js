54b2794b13e10cf74eb3590f45f935ccc000a334
lojpark <lojpark@gmail.com>
2019-02-07 02:42:45 +0900

    Show posting layout when only user signed in

--- src/components/album/AlbumTopPosting.js
+++ src/components/album/AlbumTopPosting.js
@@ -41,6 +41,10 @@ class AlbumTopPosting extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.createPost(this.state);
+        this.setState({
+            title: '',
+            content: ''
+        })
     };
 
     /**

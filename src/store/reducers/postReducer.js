a2b52a9d6ba3cc3c1cab1d887cbd2078c5bc9a9f
lojpark <lojpark@gmail.com>
2019-02-06 14:25:26 +0900

    Create postReducer

---
+++ src/store/reducers/postReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    posts: [
+        {id: '1', title: 'first', content: 'get cape'},
+        {id: '2', title: 'second', content: 'wear cape'},
+        {id: '3', title: 'third', content: 'fly'}
+    ]
+}
+
+const postReducer = (state = initState, action) => {
+    return state;
+}

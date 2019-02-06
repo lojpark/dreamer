c15e6072b192ad62dd628fad110af77232d697be
lojpark <lojpark@gmail.com>
2019-02-06 16:44:30 +0900

    Remove album reducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import albumReducer from './albumReducer'
 import authReducer from './authReducer'
 import postReducer from './postReducer'
 import profileReducer from './authReducer'
 
 const rootReducer = combineReducers({
-    album: albumReducer,
     auth: authReducer,
     post: postReducer,
 })

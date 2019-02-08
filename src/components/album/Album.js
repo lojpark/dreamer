1ce2c64579ac5bb80db662e31842b94a9c1c2804
lojpark <lojpark@gmail.com>
2019-02-08 19:45:11 +0900

    Show post as a dialog

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -12,10 +12,15 @@ import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
-const styles = theme => (
-  {
-  
+
+const styles = theme => ({
   icon: {
     marginRight: theme.spacing.unit * 2,
   },
@@ -32,7 +37,6 @@ const styles = theme => (
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
   },
-
 });
 
 // const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -40,16 +44,22 @@ const styles = theme => (
 class Album extends React.Component {
   state = {
     viewPost: null,
+    open: false,
   };
 
-  // Get state from child (AlbumItem)
+  // Get post from child (AlbumItem)
   myCallback = (dataFromChild) => {
-    console.log(dataFromChild);
-
     this.setState({
-      viewPost: dataFromChild
+      viewPost: dataFromChild,
+      open: true,
     });
   };
+
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  }
 
   render() {
     const { classes, posts, auth } = this.props;
@@ -58,20 +68,43 @@ class Album extends React.Component {
         <CssBaseline />
         <main>
           {auth.uid ? <AlbumTopPosting /> : <AlbumTop />}
-          
+
           <div className={classNames(classes.layout, classes.cardGrid)}>
             {/* End hero unit */}
             <Grid container spacing={40}>
               {posts.map(post => {
                 return (
-                  <AlbumItem card={post.id} post={post} key={post.id} callbackFromParent={this.myCallback}/>
+                  <AlbumItem card={post.id} post={post} key={post.id} callbackFromParent={this.myCallback} />
                 )
               })}
             </Grid>
           </div>
-          <div dangerouslySetInnerHTML={
-            {__html: (this.state.viewPost ? this.state.viewPost.content : '')} }>
-          </div>
+
+          <Dialog
+            open={this.state.open}
+            onClose={this.handleClose}
+            scroll='paper'
+            aria-labelledby="scroll-dialog-title"
+          >
+            <DialogTitle id="scroll-dialog-title">
+              { this.state.viewPost ? this.state.viewPost.title : '' }
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                <div dangerouslySetInnerHTML={
+                  { __html: (this.state.viewPost ? this.state.viewPost.content : '') }
+                } />
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={this.handleClose} color="primary">
+                Subscribe
+              </Button>
+            </DialogActions>
+          </Dialog>
         </main>
         <Footer />
       </React.Fragment>
@@ -88,14 +121,13 @@ const mapStateToProps = (state) => {
   if (state.firestore.ordered.posts) {
     return {
       posts: state.firestore.ordered.posts,
-      auth : state.firebase.auth,
+      auth: state.firebase.auth,
     }
   }
-  else
-  {
+  else {
     return {
       posts: state.post.posts,
-      auth : state.firebase.auth,
+      auth: state.firebase.auth,
     }
   }
 }
